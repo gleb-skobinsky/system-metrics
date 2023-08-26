e19c2ef29ab320ac5acf160c46b6108b5863f024
gleb-skobinsky <glebgytnik@gmail.com>
2023-08-26 23:06:15 +0300

    Display temperature info

--- src/temperature.rs
+++ src/temperature.rs
@@ -1,6 +1,8 @@
 use sysinfo::{ComponentExt, System, SystemExt};
 
 pub fn init_temp(sys: &mut System, temperature_chart: &mut Vec<Vec<f32>>) {
+    sys.refresh_components_list();
+    sys.refresh_components();
     let sensor_data = sys.components();
     for component in sensor_data {
         let mut vector: Vec<f32> = Vec::with_capacity(20);
@@ -13,7 +15,11 @@ pub fn init_temp(sys: &mut System, temperature_chart: &mut Vec<Vec<f32>>) {
 }
 
 pub fn update_temp(sys: &mut System, temperature_chart: &mut Vec<Vec<f32>>) {
+    sys.refresh_components();
     let sensor_data = sys.components();
+    for component in sensor_data {
+        println!("{}", component.label().to_string())
+    }
     for (i, component) in sensor_data.iter().enumerate() {
         let temperature = component.temperature();
         temperature_chart[i].remove(0);
